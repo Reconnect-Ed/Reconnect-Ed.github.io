2144c50f1e238554a5d3f2abd45491a640d84225
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2022-09-26 21:21:10 -0700

    clean up svg

--- site/.vitepress/config.ts
+++ site/.vitepress/config.ts
@@ -77,7 +77,7 @@ export default defineConfig({
 			},
 			{
 				icon: {
-					svg: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 280.3 242" enable-background="new 0 0 280.3 242" xml:space="preserve"><g id="XMLID_1_">	<path id="XMLID_6_" d="M133.7,76H118v90h14.7c30.9,0,45.1-18.1,45.1-45C177.8,90.9,164.9,76,133.7,76z"/>	<path id="XMLID_9_" d="M210.2,0H70.1L0,121l70.1,121h140.1l70.1-121L210.2,0z M132.7,195H89V47h45.8c42.1,0,73.3,20.1,73.3,74   C208.1,172.8,170.6,195,132.7,195z"/></g></svg>',
+					svg: '<svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 280.3 242"><title>Devpost</title><g><path d="M133.7,76H118v90h14.7c30.9,0,45.1-18.1,45.1-45C177.8,90.9,164.9,76,133.7,76z"/><path d="M210.2,0H70.1L0,121l70.1,121h140.1l70.1-121L210.2,0z M132.7,195H89V47h45.8c42.1,0,73.3,20.1,73.3,74 C208.1,172.8,170.6,195,132.7,195z"/></g></svg>',
 				},
 				link: 'https://devpost.com/',
 			},
